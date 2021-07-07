e068135ba3a30fa079d3b122d1b4a2fac782458c
Ivan Contreras <ivanc.contre@gmail.com>
2021-07-07 18:53:21 -0400

    docs agregado

---
+++ docs/precache-manifest.8964ad3bf9ef33835ccce48f9202ac4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e239b86516def26c93ec548baaf73e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02ff3705524629d3f2d8",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "bc0d5a8e1082cea181a6",
+    "url": "/static/css/main.75951857.chunk.css"
+  },
+  {
+    "revision": "02ff3705524629d3f2d8",
+    "url": "/static/js/2.b5cc801f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b5cc801f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc0d5a8e1082cea181a6",
+    "url": "/static/js/main.86d9f88a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
